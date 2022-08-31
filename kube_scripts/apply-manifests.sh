e6ac47a891a817c570ff53d8f62165298d40aa06
popenc <i.nickpope@gmail.com>
2022-08-31 16:23:46 -0400

    updates for dev-aws deploy

---
+++ kube_scripts/apply-manifests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+envsubst < k8s/cyanweb-configmap.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-celery-deployment.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-flask-deployment.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-flask-service.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-mysql-service.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-mysql-sts.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-nginx-deployment.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-nginx-service.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-redis-deployment.yml | kubectl apply -f -
+envsubst < k8s/cyanweb-redis-service.yml | kubectl apply -f -
